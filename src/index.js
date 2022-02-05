25bde5a7dfd4e975b39bc5ca4fcb0346e8867a24
Mo Elwekil <Mo@descod.net>
2022-02-05 20:10:26 +0000

    react jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React
+import React from 'react'
+// import ReactDOM
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//function
+function emailHelpMessage(message) {
+    return message;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// create a functional component
+const App = () => {
+    // variable for the email address label
+    const emailAddressLabel = 'Email Address';
+    return (
+        <div>
+            <form style={{ maxWidth: 500, margin: '0 auto', marginTop: 30 }}>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">{emailAddressLabel}</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">{emailHelpMessage("We'll never share your email with anyone else.")}</div>
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+// display the content at the home page
+ReactDOM.render(<App />, document.getElementById('root'))
